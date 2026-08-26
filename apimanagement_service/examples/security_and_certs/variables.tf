d7bfad648169014e11c03e75b4226460fc68cbbc
krazouk-fr <krazouk.fr@gmail.com>
2026-08-26 10:39:34 +0200

    Add apimanagement_service/examples/security_and_certs/variables.tf

---
+++ apimanagement_service/examples/security_and_certs/variables.tf
@@ -0,0 +1,21 @@
+variable "publisher_email" {
+  type        = string
+  description = "The email address of the publisher."
+}
+
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see <https://aka.ms/avm/telemetryinfo>.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+}
+
+variable "location" {
+  type        = string
+  default     = "eastus2"
+  description = "Azure region where the resource should be deployed."
+  nullable    = false
+}
