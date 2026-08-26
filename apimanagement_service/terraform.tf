2c3b9ed1e37e2999f8a38605491cfbaf892d724f
krazouk-fr <krazouk.fr@gmail.com>
2026-08-26 10:38:40 +0200

    Add apimanagement_service/terraform.tf

---
+++ apimanagement_service/terraform.tf
@@ -0,0 +1,23 @@
+
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0, < 5.0"
+    }
+    modtm = {
+      source  = "Azure/modtm"
+      version = ">= 0.3, < 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5, < 4.0"
+    }
+  }
+}
