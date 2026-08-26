378071bb7cc771c255d7ce0c6b6ed61a648b6eb2
krazouk-fr <krazouk.fr@gmail.com>
2026-08-26 10:39:01 +0200

    Add apimanagement_service/examples/system_assigned_identity/main.tf

---
+++ apimanagement_service/examples/system_assigned_identity/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+  }
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = var.location
+  name     = module.naming.resource_group.name_unique
+}
+
+# This is the module call
+# Leaving location as `null` will cause the module to use the resource group location
+# with a data source.
+module "test" {
+  source = "../../"
+
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.api_management.name_unique
+  publisher_email     = var.publisher_email
+  resource_group_name = azurerm_resource_group.this.name
+  enable_telemetry    = var.enable_telemetry
+  managed_identities = {
+    system_assigned = true
+  }
+  publisher_name = "Apim Example Publisher"
+  sku_name       = "Premium_3"
+  tags = {
+    environment = "test"
+    cost_center = "test"
+  }
+  zones = ["1", "2", "3"] # For compliance with WAF
+}
